ae6654064c36ef1f0ca50839024eb6331f64485c
Mircea Eliade <roszcz@gmail.com>
2018-05-12 14:21:08 +0000

    pipe data from dh to influx to grafana

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from devicehive import Handler
 from devicehive import DeviceHive
+from influxdb import InfluxDBClient
+from datetime import datetime as dt
+
+def notification2points(notification):
+    device_id = notification.device_id
+    value = notification.parameters['tick'] % 100
+    print(value)
+
+    points = [{
+        "measurement": '{}'.format(device_id),
+        # "time": int(now.strftime('%s')),
+        "time": dt.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "fields": {
+            "value": value
+        }
+    }]
+
+    return points
 
 class SimpleHandler(Handler):
+    def __init__(self, *args, **kwargs):
+        super(SimpleHandler, self).__init__(*args, **kwargs)
+
+        # Influx access
+        host = 'localhost'
+        port = 8086
+        USER = 'root'
+        PASSWORD = 'root'
+        DBNAME = 'tutorial'
+        self.db = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
+
     def handle_connect(self):
         device_ids = ['esp-mon1']
         for device_id in device_ids:
@@ -17,9 +46,8 @@ class SimpleHandler(Handler):
         print(command.command)
 
     def handle_notification(self, notification):
-        print(notification.device_id)
-        print(notification.parameters['tick'])
-        print('--')
+        points = notification2points(notification)
+        self.db.write_points(points)
 
 if __name__ == '__main__':
     url = 'http://35.227.31.21/api/rest'
